88157b9d9026479cdab97d8965b8b8be7cdcd5bc
Paweł Stiasny <pawelstiasny@gmail.com>
2013-06-01 21:28:14 +0200

    Intensification filter.

--- fuzzy.py
+++ fuzzy.py
@@ -1,3 +1,5 @@
+import numpy
+
 def fuzzify(raw_rgb):
 	# normalizacja z rozciagnieciem histogramu 
 	rgbmin = raw_rgb.min()
@@ -9,4 +11,11 @@ def defuzzify(membership, original_rgb):
 	return membership
 
 def membership_pass(membership):
-	pass
+	return intensify(membership)
+
+def intensify(membership, threshold = 0.5):
+	res = numpy.zeros_like(membership)
+	res[membership <= 0.5] = 2 * membership[membership <= 0.5]**2
+	res[membership > 0.5] = 1 - 2 * (1.0 - membership[membership > 0.5])**2
+	return res
+
